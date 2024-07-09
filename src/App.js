15c8115ba41903171e42159eaa7a2a0c70210791
hiago rodrigues <ti7@facfama.edu.br>
2024-07-09 10:43:43 -0300

    att jogo acaba e retonar novo jogo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Pergunta from './pergunta';
 import Resultado from './resultado';
@@ -6,17 +6,47 @@ import data from './perguntas.json';
 import backgroundImage from './background.jpg';
 
 function App() {
+  const [perguntas, setPerguntas] = useState([]);
   const [perguntaIndex, setPerguntaIndex] = useState(0);
   const [mostrarResultado, setMostrarResultado] = useState(false);
   const [respostaCorreta, setRespostaCorreta] = useState(false);
+  const [perguntasFeitas, setPerguntasFeitas] = useState([]);
+
+  // Função para embaralhar as perguntas
+  const embaralharPerguntas = (perguntas) => {
+    for (let i = perguntas.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [perguntas[i], perguntas[j]] = [perguntas[j], perguntas[i]];
+    }
+    return perguntas;
+  };
+
+  // Efeito para inicializar o jogo
+  useEffect(() => {
+    // Embaralha as perguntas quando o componente monta
+    setPerguntas(embaralharPerguntas([...data]));
+    setPerguntaIndex(0);
+    setMostrarResultado(false);
+    setRespostaCorreta(false);
+    setPerguntasFeitas([]);
+  }, [data]); // Executa sempre que `data` muda
 
   const proximaPergunta = () => {
-    setPerguntaIndex(perguntaIndex + 1);
+    // Adiciona a pergunta atual às perguntas feitas
+    setPerguntasFeitas([...perguntasFeitas, perguntaIndex]);
+    
+    // Verifica se ainda há perguntas não feitas
+    if (perguntasFeitas.length + 1 < perguntas.length) {
+      setPerguntaIndex(perguntaIndex + 1);
+    } else {
+      // Se todas as perguntas foram feitas, encerra o jogo
+      setPerguntaIndex(-1); // Sinaliza o fim do jogo
+    }
     setMostrarResultado(false);
   };
 
   const handleResposta = (respostaSelecionada) => {
-    const perguntaAtual = data[perguntaIndex];
+    const perguntaAtual = perguntas[perguntaIndex];
     if (respostaSelecionada === perguntaAtual.resposta_correta) {
       setRespostaCorreta(true);
     } else {
@@ -24,6 +54,16 @@ function App() {
     }
     setMostrarResultado(true);
   };
+
+  if (perguntaIndex === -1) {
+    // Jogo encerrado
+    return (
+      <div>
+        <h1>Jogo Encerrado!</h1>
+        <p>Você respondeu todas as perguntas.</p>
+      </div>
+    );
+  }
 
   return (
     <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
