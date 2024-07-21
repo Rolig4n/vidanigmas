bd1274d92841ce289c957f26629231fe9d077542
Hiago PC <hiagok.o@gmail.com>
2024-07-21 18:16:36 -0300

    att css da carta

--- src/pergunta.js
+++ src/pergunta.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import styles from './styles.module.css'
 
 const Pergunta = ({ pergunta, mostrarResultado }) => {
-  const { pergunta: textoPergunta, resposta_correta } = pergunta;
+  const { pergunta: textoPergunta, resposta_correta, programa } = pergunta;
 
   return (
     <div>
       <h2>{textoPergunta}</h2>
       <h1>{mostrarResultado ? resposta_correta : ''}</h1>
+      <p className={styles.programa}>ep.{programa}</p>
     </div>
   );
 };
